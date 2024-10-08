d73bc00afd4fa70297cb5c0d5a99a340d7083b1e
barbatos2011 <aaron.barbatos@outlook.com>
2024-10-08 12:00:44 +0800

    add tools

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -230,8 +230,8 @@ public class FullNode {
 ////        long endBlock = 65352295L;
 //        long endBlock = 64805095L;
 
-        long startBlock = 65726250L;
-        long endBlock = 65755055L;
+        long startBlock = 65755050L;
+        long endBlock = 65783855L;
         final String OWNER_ADDRESS = "TPsUGKAoXDSFz332ZYtTGdDHWzftLYWFj7";
         final String CONTRACT_ADDRESS = "TZFs5ch1R1C4mmjwrrmZqeqbUgGpxY1yWB";
 
